b85d5b12ec871efa67a55ac9210d5b154be26b4e
Kai Chang <kaijchang@gmail.com>
2018-10-15 20:45:47 -0700

    make setup.py work

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools.config import read_configuration
 
+import os
 
-setup()
+setup(**read_configuration(os.path.join(
+    os.path.dirname(__file__), 'setup.cfg'))['options'])
